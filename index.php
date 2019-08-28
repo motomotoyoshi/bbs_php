88e49668bbd678d01aae4aa807c415b2d1315fc1
motomotoyoshi <motomoto.git@gmail.com>
2019-08-28 23:20:57 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>簡易掲示板</title>
+</head>
+<body>
+    <h1>簡易掲示板</h1>
+    <form>
+        message: <input type="text" name="message">
+        user: <input type="text" name="user">
+    </form>
+    <h2>投稿一覧 （0件）</h2>
+    <ul>
+        <li>まだ投稿はありません。</li>
+    </ul>
+</body>
+</html>
